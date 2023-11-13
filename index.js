6b2a5a36460858101de8a0bfa05414fd177d8b5d
alishabab62 <shababali672@gmail.com>
2023-11-13 14:13:54 +0530

    round count added

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 const startBtn = document.getElementById("startBtn");
 const options = document.getElementById("your-options");
 const modalContainer = document.getElementById("modal-container")
+const roundContainer = document.getElementById('round-container');
+let computerRound = 0;
+let userWin = 0;
+
+const comRoundOutput = document.createElement("h3");
+const userRoundOutput = document.createElement("h3");
+comRoundOutput.innerHTML = `Computer win: ${computerRound}`;
+userRoundOutput.innerHTML = `User win: ${userWin}`;
+roundContainer.appendChild(comRoundOutput);
+roundContainer.appendChild(userRoundOutput);
+
+
+
 let computerChoice;
 startBtn.addEventListener("click",() => {
     const computerOptions = [1,2,3];
@@ -12,36 +25,95 @@ startBtn.addEventListener("click",() => {
 
 
 let reloadBtn = document.createElement('button');
+let output3 = document.createElement('h3');
 
 options.addEventListener("change",() => {
     const selectedIndexUser = options.selectedIndex;
-    console.log(selectedIndexUser,computerChoice);
     options.style.display = "none";
     if(selectedIndexUser == 0){
         alert("Please select valid Choice");
         return;
     }
     if(selectedIndexUser == computerChoice){
-        alert("Match Draw");
+        modalContainer.innerHTML = ""
+        output3.innerHTML = "Match Draw";
+        modalContainer.appendChild(output3);
+        
     }
     else if(selectedIndexUser == 1 && computerChoice == 2){
-        alert("Computer Win");
+        modalContainer.innerHTML = ""
+        output3.innerHTML = "Computer Win";
+        modalContainer.appendChild(output3);
+        computerRound++;
+        roundContainer.innerHTML = "";
+        comRoundOutput.innerHTML = `Computer win: ${computerRound}`;
+        userRoundOutput.innerHTML = `User win: ${userWin}`;
+        roundContainer.appendChild(comRoundOutput);
+        roundContainer.appendChild(userRoundOutput);
+
     }
     else if(selectedIndexUser == 1 && computerChoice == 3){
-        alert("You Win");
+        modalContainer.innerHTML = ""
+        output3.innerHTML = "You Win";
+        modalContainer.appendChild(output3);
+        userWin++;
+        roundContainer.innerHTML = ""
+        comRoundOutput.innerHTML = `Computer win: ${computerRound}`;
+        userRoundOutput.innerHTML = `User win: ${userWin}`;
+        roundContainer.appendChild(comRoundOutput);
+        roundContainer.appendChild(userRoundOutput);
+
     }
     else if(selectedIndexUser == 2 && computerChoice == 1){
-        alert("You Win");
+        modalContainer.innerHTML = ""
+        output3.innerHTML = "You Win";
+        modalContainer.appendChild(output3);
+        userWin++;
+        roundContainer.innerHTML = ""
+        comRoundOutput.innerHTML = `Computer win: ${computerRound}`;
+        userRoundOutput.innerHTML = `User win: ${userWin}`;
+        roundContainer.appendChild(comRoundOutput);
+        roundContainer.appendChild(userRoundOutput);
+        
     }
     else if(selectedIndexUser == 2 && computerChoice == 3){
-        alert("Computer Win");
+        modalContainer.innerHTML = ""
+        output3.innerHTML = "Computer Win";
+        modalContainer.appendChild(output3);
+        computerRound++;
+        roundContainer.innerHTML = ""
+        comRoundOutput.innerHTML = `Computer win: ${computerRound}`;
+        userRoundOutput.innerHTML = `User win: ${userWin}`;
+        roundContainer.appendChild(comRoundOutput);
+        roundContainer.appendChild(userRoundOutput);
+        
     }
     else if(selectedIndexUser == 3 && computerChoice == 1){
-        alert("Computer Win");
+        modalContainer.innerHTML = ""
+        output3.innerHTML = "Computer Win";
+        modalContainer.appendChild(output3);
+        computerRound++;
+        roundContainer.innerHTML = ""
+        comRoundOutput.innerHTML = `Computer win: ${computerRound}`;
+        userRoundOutput.innerHTML = `User win: ${userWin}`;
+        roundContainer.appendChild(comRoundOutput);
+        roundContainer.appendChild(userRoundOutput);
+        
     }
     else if(selectedIndexUser == 3 && computerChoice == 2){
-        alert("You Win");
+        modalContainer.innerHTML = ""
+        output3.innerHTML = "You Win";
+        modalContainer.appendChild(output3);
+        userWin++;
+        roundContainer.innerHTML = ""
+        comRoundOutput.innerHTML = `Computer win: ${computerRound}`;
+        userRoundOutput.innerHTML = `User win: ${userWin}`;
+        roundContainer.appendChild(comRoundOutput);
+        roundContainer.appendChild(userRoundOutput);
+        
     }
+
+
     modalContainer.style.display = "block";
     const output1 = document.createElement('h3');
     const output2 = document.createElement('h3');
@@ -58,5 +130,6 @@ options.addEventListener("change",() => {
 
 
 reloadBtn.addEventListener('click', () =>{
-    location.reload();
-})
+    modalContainer.style.display = "none";
+});
+
